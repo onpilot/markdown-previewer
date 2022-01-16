4573ef23a27aff50932ee49e51e3c2a94b5cc2b5
onpilot <72357530+onpilot@users.noreply.github.com>
2022-01-16 22:37:49 +0700

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
-import './App.css';
+import React from 'react';
 
 function App() {
-  return <div className="App"></div>;
+  const [value, setValue] = React.useState('');
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  };
+  return (
+    <>
+      <textarea
+        name="editor"
+        id="editor"
+        cols="30"
+        rows="10"
+        value={value}
+        onChange={handleChange}
+      ></textarea>
+      <div id="preview">{value}</div>
+    </>
+  );
 }
 
 export default App;
