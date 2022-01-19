87f36ff941dd8697ca027c14d572551228d9e6f8
onpilot <72357530+onpilot@users.noreply.github.com>
2022-01-20 00:38:48 +0700

    fetching sample text from marked.js demo file using built-in browser Fetch API and useEffect hook

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { marked } from 'marked';
 import DOMPurify from 'dompurify';
 
@@ -13,14 +13,49 @@ const sanitizer = (dirty) => {
 
 function App() {
   const [value, setValue] = React.useState('');
+  const [loading, setLoading] = React.useState(true);
+  const [error, setError] = React.useState(null);
+
   const handleChange = (event) => {
     setValue(event.target.value);
   };
+
   const getRawMarkup = () => {
     const compiledMarkdown = parseMarkdown(value);
     const clean = sanitizer(compiledMarkdown);
     return { __html: clean };
   };
+
+  useEffect(() => {
+    fetch(
+      'https://raw.githubusercontent.com/markedjs/marked/e5796ecc435a30f96939e6a7b2229c14264b4bf8/docs/demo/initial.md'
+    )
+      .then((response) => {
+        // console.log(response);
+        if (response.ok) {
+          return response.text();
+        }
+        throw response;
+      })
+      .then((text) => {
+        setValue(text);
+      })
+      .catch((error) => {
+        console.error('Error fetching sample text: ', error);
+        setError(error);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) {
+    return 'Loading...';
+  }
+  if (error) {
+    return 'Error!';
+  }
+
   return (
     <>
       <textarea
